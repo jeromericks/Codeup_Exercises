e119f52792e40bd0a495fadf22b84bdd5b382c4c
jeromericks <jeromericks@gmail.com>
2015-11-05 01:02:48 -0600

    Added Foreach assignment

--- foreach.php
+++ foreach.php
@@ -1,5 +1,35 @@
 <?php
 
+$things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
+foreach ($things as $thing) {
+	if (is_integer($thing)) {
+		echo ("$thing's type is integer" . PHP_EOL);
+	} elseif (is_float($thing)) {
+		echo ("$thing's type is float" . PHP_EOL);
+	} elseif (is_bool($thing)) {
+		echo ("$thing's type is boolean" . PHP_EOL);
+	} elseif (is_array($thing)) {
+		foreach ($thing as $element) {
+			echo ("$element is part of an array" . PHP_EOL);
+		}
+	} elseif (is_null($thing)) {
+		echo ("$thing type is null" . PHP_EOL);
+	} elseif (is_string($thing)) {
+		echo ("$thing's type is string" . PHP_EOL);
+	}
+}
+
+foreach ($things as $thing) {
+	if (is_scalar($thing)) {
+		echo ("$thing is scalar" . PHP_EOL);
+	} 
+}
+
+foreach ($things as $thing) {
+	if (is_array($thing)) {
+		echo (implode(', ', $thing) . PHP_EOL);
+	}
+}
 
 ?>
