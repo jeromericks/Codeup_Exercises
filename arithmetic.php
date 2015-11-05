8114181e2303834dd4c96f2f8d1d52c37772d530
jeromericks <jeromericks@gmail.com>
2015-11-05 11:43:22 -0600

    Added arithmetic functions

--- arithmetic.php
+++ arithmetic.php
@@ -1,25 +1,35 @@
 <?php
 
-function add($a, $b)
-{
-    echo $a + $b;
+$a = 5;
+$b = 4;
+
+function add($a, $b) {
+    echo $a + $b . PHP_EOL;
 }
 
-function subtract($a, $b)
-{
-    // Add code here
+function subtract($a, $b) {
+    echo $a - $b . PHP_EOL;
 }
 
-function multiply($a, $b)
-{
-    // Add code here
+function multiply($a, $b) {
+    echo $a * $b . PHP_EOL;
 }
 
-function divide($a, $b)
-{
-    // Add code here
+function divide($a, $b) {
+    echo $a / $b . PHP_EOL;
 }
+
+function modulus($a, $b) {
+	echo $a % $b . PHP_EOL;
+}
+
 
 // Add code to test your functions here
+add($a, $b);
+subtract($a, $b);
+multiply($a, $b);
+divide($a, $b);
+modulus($a, $b);
+
 
 ?>
