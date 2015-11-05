2b9f3d9630142b94eae99fed9169d48220d6db06
jeromericks <jeromericks@gmail.com>
2015-11-05 14:46:26 -0600

    Error function is functional now

--- arithmetic.php
+++ arithmetic.php
@@ -1,50 +1,58 @@
 <?php
 
 $a = 5;
-$b = 0;
+$b = 4;
 
 function add($a, $b) {
-	if (is_numeric($a) && is_numeric($b)) {
-    	return $a + $b . PHP_EOL;
-    } else {
-        error($a, $b);
-    }
+	$error = errorCheck($a, $b);
+	if($error) {
+		return $a + $b . PHP_EOL;
+	}
 }
 
 function subtract($a, $b) {
-	if (is_numeric($a) && is_numeric($b)) {
-    	return $a - $b . PHP_EOL;
-    } else {
-        error($a, $b);
-    }
+	$error = errorCheck($a, $b);
+	if($error) {
+		return $a - $b . PHP_EOL;
+	}
 }
 
 function multiply($a, $b) {
-	if (is_numeric($a) && is_numeric($b)) {
-    	return $a * $b . PHP_EOL;
-    } else {
-        error($a, $b);
-    }
+	$error = errorCheck($a, $b);
+	if($error) {
+		return $a * $b . PHP_EOL;
+	}
 }
 
 function divide($a, $b) {
-	if (is_numeric($a) && is_numeric($b) && $a !== 0 && $b !== 0) {
-    	return $a / $b . PHP_EOL;
-    } else {
-        error($a, $b);
-    }
+	$error = errorCheck($a, $b);
+	if($error) {
+		if($a == 0 || $b == 0) {
+			return "ERROR: cannot use zero as a variable" . PHP_EOL;
+		} else {
+			return $a / $b . PHP_EOL;
+		}
+	}
 }
 
 function modulus($a, $b) {
-	if (is_numeric($a) && is_numeric($b) && $a !== 0 && $b !== 0) {
-		return $a % $b . PHP_EOL;
-	} else {
-        error($a, $b);
-    }
+	$error = errorCheck($a, $b);
+	if($error) {
+		if($a == 0 || $b == 0) {
+			return "ERROR: cannot use zero as a variable" . PHP_EOL;
+		} else {
+			return $a % $b . PHP_EOL;
+		}
+	}
 }
 
-function error($a, $b) {
-	return "ERROR: {$a} and {$b} must be numbers and you can't use zero" . PHP_EOL;
+function errorCheck($a, $b) {
+	if(!is_numeric($a) || !is_numeric($b)) {
+		echo "ERROR: {$a} and {$b} must be numbers" . PHP_EOL;
+		return false;
+	} else {
+		return true;
+	}
 }
 
 
