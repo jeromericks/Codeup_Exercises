8e9f956bc54a7fa006710e50a90f50a2f5aeb424
jeromericks <jeromericks@gmail.com>
2015-11-05 08:40:59 -0600

    Added brackets around variables for better organization

--- foreach.php
+++ foreach.php
@@ -4,19 +4,19 @@ $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, 
 
 foreach ($things as $thing) {
 	if (is_integer($thing)) {
-		echo ("$thing's type is integer" . PHP_EOL);
+		echo ("{$thing}'s type is integer" . PHP_EOL);
 	} elseif (is_float($thing)) {
-		echo ("$thing's type is float" . PHP_EOL);
+		echo ("{$thing}'s type is float" . PHP_EOL);
 	} elseif (is_bool($thing)) {
-		echo ("$thing's type is boolean" . PHP_EOL);
+		echo ("{$thing}'s type is boolean" . PHP_EOL);
 	} elseif (is_array($thing)) {
 		foreach ($thing as $element) {
-			echo ("$element is part of an array" . PHP_EOL);
+			echo ("{$element} is part of an array" . PHP_EOL);
 		}
 	} elseif (is_null($thing)) {
-		echo ("$thing type is null" . PHP_EOL);
+		echo ("{$thing} type is null" . PHP_EOL);
 	} elseif (is_string($thing)) {
-		echo ("$thing's type is string" . PHP_EOL);
+		echo ("{$thing}'s type is string" . PHP_EOL);
 	}
 }
 
@@ -25,7 +25,7 @@ echo (PHP_EOL);
 
 foreach ($things as $thing) {
 	if (is_scalar($thing)) {
-		echo ("$thing is scalar" . PHP_EOL);
+		echo ("{$thing} is scalar" . PHP_EOL);
 	} 
 }
 
