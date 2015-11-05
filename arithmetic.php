a254beb0a492adc57c27f3abf7af735c540e2a47
jeromericks <jeromericks@gmail.com>
2015-11-05 14:13:24 -0600

    Edited break and continue and started the error function

--- arithmetic.php
+++ arithmetic.php
@@ -1,35 +1,59 @@
 <?php
 
 $a = 5;
-$b = 4;
+$b = 0;
 
 function add($a, $b) {
-    echo $a + $b . PHP_EOL;
+	if (is_numeric($a) && is_numeric($b)) {
+    	return $a + $b . PHP_EOL;
+    } else {
+        error($a, $b);
+    }
 }
 
 function subtract($a, $b) {
-    echo $a - $b . PHP_EOL;
+	if (is_numeric($a) && is_numeric($b)) {
+    	return $a - $b . PHP_EOL;
+    } else {
+        error($a, $b);
+    }
 }
 
 function multiply($a, $b) {
-    echo $a * $b . PHP_EOL;
+	if (is_numeric($a) && is_numeric($b)) {
+    	return $a * $b . PHP_EOL;
+    } else {
+        error($a, $b);
+    }
 }
 
 function divide($a, $b) {
-    echo $a / $b . PHP_EOL;
+	if (is_numeric($a) && is_numeric($b) && $a !== 0 && $b !== 0) {
+    	return $a / $b . PHP_EOL;
+    } else {
+        error($a, $b);
+    }
 }
 
 function modulus($a, $b) {
-	echo $a % $b . PHP_EOL;
+	if (is_numeric($a) && is_numeric($b) && $a !== 0 && $b !== 0) {
+		return $a % $b . PHP_EOL;
+	} else {
+        error($a, $b);
+    }
+}
+
+function error($a, $b) {
+	return "ERROR: {$a} and {$b} must be numbers and you can't use zero" . PHP_EOL;
 }
 
 
 // Add code to test your functions here
-add($a, $b);
-subtract($a, $b);
-multiply($a, $b);
-divide($a, $b);
-modulus($a, $b);
+echo(add($a, $b));
+echo(subtract($a, $b));
+echo(multiply($a, $b));
+echo(divide($a, $b));
+echo(modulus($a, $b));
 
 
 ?>
