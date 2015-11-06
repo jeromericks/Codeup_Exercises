15daf117fa775fc5d6e40dc16f3662876851cd11
jeromericks <jeromericks@gmail.com>
2015-11-06 09:25:02 -0600

    Added Technology companies assignment

---
+++ technology-companies.php
@@ -0,0 +1,71 @@
+<?php
+
+
+$companies = [
+    'Sun Microsystems' => [
+        'Vinod Khosla',
+        'Bill Joy',
+        'Andy Bechtolsheim',
+        'Scott McNealy'
+    ],
+    'Silicon Graphics' => [
+        'Jim Clark',
+        'Ed McCracken'
+    ],
+    'Cray' => [
+        'William Norris',
+        'Seymour Cray'
+    ],
+    'NeXT' => [
+        'Steve Jobs',
+        'Avie Tevanian',
+        'Joanna Hoffman',
+        'Bud Tribble',
+        'Susan Kare'
+    ],
+    'Acorn Computers' => [
+        'Steve Furber',
+        'Sophie Wilson',
+        'Hermann Hauser',
+        'Jim Mitchell'
+    ],
+    'MIPS Technologies' => [
+        'Skip Stritter',
+        'John L. Hennessy'
+    ],
+    'Commodore' => [
+        'Yash Terakura',
+        'Bob Russell',
+        'Bob Yannes',
+        'David A. Ziembicki',
+        'Jay Miner'
+    ],
+    'Be Inc' => [
+        'Steve Sakoman',
+        'Jean-Louis Gassée'
+    ]
+];
+
+print_r($companies);
+
+echo PHP_EOL;
+
+ksort($companies);
+print_r($companies);
+
+echo PHP_EOL;
+
+foreach($companies as $company) {
+    echo "Company" . PHP_EOL;
+    sort($company);
+    print_r($company);
+    echo "End" . PHP_EOL;
+}
+
+echo PHP_EOL;
+
+arsort($companies);
+print_r($companies);
+
+
+?>
