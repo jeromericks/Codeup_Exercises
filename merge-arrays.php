05cd4e59d8182a26250c41067fb3daf9997aaa18
jeromericks <jeromericks@gmail.com>
2015-11-09 12:17:07 -0600

    Corrected the order of merge arrays

--- merge-arrays.php
+++ merge-arrays.php
@@ -5,8 +5,8 @@ $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
 
-function getName($person, $names){
-	$result = array_search($person, $names); 
+function lookInArray($element, $array){
+	$result = array_search($element, $array); 
 	if ($result === false){
 		return false; 
 	} else {
@@ -14,26 +14,43 @@ function getName($person, $names){
 	}
 }
 
-echo getName('Tina', $names) . PHP_EOL;
-echo getName('Bob', $names) . PHP_EOL; 
+echo lookInArray('Tina', $names) . PHP_EOL;
+echo lookInArray('Bob', $names) . PHP_EOL; 
 
 
-function compareArray($compare, $names) {
-	$commonItems = 0;
-	foreach ($names as $key => $value) {
-		$check = array_search($value, $compare); 
-		if ($check !== false){   
-			$commonItems++;  
+function compareArray($newArray, $controlArray) {
+	$match = 0;
+	$diff = 0;
+
+	foreach ($newArray as $singleElement) {
+		if(lookInArray($singleElement, $controlArray)) {
+			$match = $match + 1;
+		} else {
+			$diff = $diff + 1;
 		}
+
+		
 	}
-	return "These arrays have {$commonItems} things in common" . PHP_EOL; 
+	return "These arrays have {$match} values in common and {$diff} that didn't match between the two arrays" . PHP_EOL; 
 }
 
 echo compareArray($compare, $names) . PHP_EOL; 
 
 
-function combine_arrays() {
+function combine_arrays($newArray, $controlArray) {
+	$thirdArray = [];
 
+	for ($i = 0; $i < count($newArray); $i++) { 
+		if($newArray[$i] == $controlArray[$i]) {
+			array_push($thirdArray, $newArray[$i]);
+		} else {
+			array_push($thirdArray, $newArray[$i]);
+			array_push($thirdArray, $controlArray[$i]);
+		}
+	}
+	print_r($thirdArray);
 }
+
+echo combine_arrays($names, $compare) . PHP_EOL;
 
 ?>
