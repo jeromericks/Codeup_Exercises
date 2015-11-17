31ead11ff842f71200fc8c680ac5249c5b9cf25e
jeromericks <jeromericks@gmail.com>
2015-11-17 15:21:17 -0600

    Completed log assignment and moved the log files to the public folder

--- Log.php
+++ Log.php
@@ -7,9 +7,9 @@ class Log
 	public function logMessage($logLevel, $message)
 	{
 		$todaysDate = date("Y-m-d");
-		$todaysDateTime = date("h:i:s A");
-		$handle = fopen($this->$filename, 'a');
-		$formattedDate = $todaysDate . ' ' . $todaysDateTime . ' ' . $logLevel . ' ' . $message . PHP_EOL;
+		$todaysTime = date("h:i:s A");
+		$handle = fopen($this->filename, 'a');
+		$formattedDate = $todaysDate . ' ' . $todaysTime . ' ' . $logLevel . ' ' . $message . PHP_EOL;
 		fwrite($handle, $formattedDate);
 		fclose($handle);
 	}
--- log_test.php
+++ log_test.php
@@ -1,10 +1,11 @@
 <?php
 
+date_default_timezone_set('America/Chicago');
 require_once 'Log.php';
 
 $log = new Log();
 
-$log->filename = "data/log-{$todaysDate}.log";
+$log->filename = "data/log-" . date("Y-m-d") . ".log";
 $log->info("This is an info message.");
 $log->error("This is an error message.");
 
