20e6f7df435c74286b773c9de843faa6f69efe63
jeromericks <jeromericks@gmail.com>
2015-12-01 11:32:02 -0600

    Completed db_connect and pdo_test

---
+++ pdo_test.php
@@ -0,0 +1,10 @@
+<?php
+
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME', 'codeup_test_db');
+define('DB_USER', 'codeup');
+define('DB_PASS', 'password');
+
+require 'db_connect.php';
+
+echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
