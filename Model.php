2bd807e7be0919ac51b390f96db01352b7bbda01
jeromericks <jeromericks@gmail.com>
2015-12-07 14:15:42 -0600

    Completed Model assignment and tested it

--- Model.php
+++ Model.php
@@ -2,5 +2,21 @@
 
 class Model
 {
-	
+	private $attributes = [];
+
+	public function __set($name, $value)
+    {
+        $this->attributes[$name] = $value;
+    }
+
+    public function __get($name)
+    {
+        if (array_key_exists($name, $this->attributes)) {
+            return $this->attributes[$name];
+        }
+
+        return null;
+    }
 }
+
+?>
---
+++ model_test.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once 'Model.php';
+
+$model = new Model();
+$model->name = 'Jerome Ricks';
+$model->class = 'Codeup';
+
+print_r($model);
+echo PHP_EOL;
+echo "Name is " . $model->name . PHP_EOL;
+echo "Class is " . $model->class . PHP_EOL;
+echo PHP_EOL;
+
+?>
