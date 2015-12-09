853604b163b95e623f06f01042c3aa349760a0aa
jeromericks <jeromericks@gmail.com>
2015-12-08 19:09:34 -0600

    Completed Model class and added test code to User class

--- Model.php
+++ Model.php
@@ -4,6 +4,28 @@ class Model
 {
 	private $attributes = [];
 	protected static $table;
+	protected static $dbc;
+
+	/*
+     * Constructor
+     */
+    public function __construct()
+    {
+        self::dbConnect();
+    }
+
+    /*
+     * Connect to the DB
+     */
+    private static function dbConnect()
+    {
+        if (!self::$dbc)
+        {
+        	require_once 'config.php';
+			require_once 'db_connect.php';
+            self::$dbc = $dbc;
+        }
+    }
 
 	public function __set($name, $value)
     {
@@ -19,10 +41,135 @@ class Model
         return null;
     }
 
+    /*
+     * Persist the object to the database
+     */
+    public function save()
+    {
+  
+        // @TODO: Ensure there are attributes before attempting to save
+    	if(!empty($this->attributes['id']) ) {
+    		if (isset($this->attributes['id'])) {
+	    		$this->update($this->attributes['id']);
+    		} else {
+    			$this->insert();
+    		}
+    	}
+        // @TODO: Perform the proper action - if the `id` is set, this is an update, if not it is a insert
+
+        // @TODO: Ensure that update is properly handled with the id key
+
+        // @TODO: After insert, add the id back to the attributes array so the object can properly reflect the id
+
+        // @TODO: You will need to iterate through all the attributes to build the prepared query
+
+        // @TODO: Use prepared statements to ensure data security
+    }
+
+    protected function insert()
+    {
+    	$newKeysArray = [];
+    	$keysArray = array_keys($this->attributes);
+    	$table = static::$table;
+
+    	$insert_table = "INSERT INTO $table (";
+    	$insert_table .= implode(', ', $keysArray);
+    	$insert_table .= ") VALUES (";
+
+    	foreach($keysArray as $key) {
+    		$newKeysArray[] = ':' . $key;
+    	}
+
+    	$insert_table .= implode(', ', $newKeysArray);
+    	$insert_table .= ");";
+
+		$stmt = self::$dbc->prepare($insert_table);
+
+		foreach($this->attributes as $key => $value) {
+			$stmt->bindValue(':' . $key, $value, PDO::PARAM_STR);
+		}
+		$stmt->execute();
+    }
+
+    protected function update($id)
+    {
+    	$updateArray = [];
+    	$table = static::$table;
+
+        foreach ($this->attributes as $key => $value) {
+            $update = $key . ' = :' . $key;
+            array_push($updateArray, $update);
+        }
+        $updateString = implode(', ', $updateArray);
+       	$updateString = "UPDATE $table SET $updateString WHERE id = :id";
+
+        $stmt = self::$dbc->prepare($updateString);
+        
+        foreach ($this->attributes as $key => $value) {
+            $stmt->bindValue(':'. $key, $this->attributes[$key], PDO::PARAM_STR);
+            $stmt->bindValue(':id', $id, PDO::PARAM_STR);
+        }
+        $stmt->execute();
+		
+    }
+    /*
+     * Find a record based on an id
+     */
+    public static function find($id)
+    {
+        // Get connection to the database
+        self::dbConnect();
+        $table = static::$table;
+
+        // @TODO: Create select statement using prepared statements
+        $query = "SELECT * FROM $table WHERE id = :id";
+
+        $stmt = self::$dbc->prepare($query);
+        $stmt->bindValue(':id', $id, PDO::PARAM_STR);
+        $stmt->execute();
+
+        // @TODO: Store the resultset in a variable named $result
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        // The following code will set the attributes on the calling object based on the result variable's contents
+        $instance = null;
+        if ($result)
+        {
+            $instance = new static;
+            $instance->attributes = $result;
+        } else {
+        	$instance = "User not found";
+        }
+        return $instance;
+    }
+
+    /*
+     * Find all records in a table
+     */
+    public static function all()
+    {
+        self::dbConnect();
+        $table = static::$table;
+
+        $query = "SELECT * FROM $table";
+
+        $stmt = self::$dbc->query($query);
+
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        // @TODO: Learning from the previous method, return all the matching records
+        return $results;
+    }
+
     public static function getTableName()
     {
     	return static::$table;
     }
+
+	public function getAttributes()
+    {
+    	return $this->attributes;
+    }
+
 }
 
 ?>
--- User.php
+++ User.php
@@ -1,10 +1,25 @@
 <?php
 
-require_once 'config.php';
-require_once 'db_connect.php';
 require_once 'Model.php';
 
 class User extends Model
 {
-	protected static $table = 'users';
+	protected static $table = 'contacts';
+
 }
+
+$user = new User();
+$user->email = 'jeromericks@gmail.com';
+$user->name = 'Jerome';
+$user->phone = '979-446-7969';
+$user->address = '100 Hi Avenue';
+$user->city = 'San Antonio';
+$user->state = 'TX';
+$user->zip = '78212';
+$user->name = 'Jerome is the greatest';
+$model = User::all();
+// $find = User::find(2);
+$test = $user->getAttributes();
+$save = $user->save();
+var_dump($model);
+// $user->save();
